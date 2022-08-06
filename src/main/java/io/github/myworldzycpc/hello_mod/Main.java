4c1a4f06b9b85e746732b82442c78a600c9c380d
myworldzycpc <myworldzhangyichen@qq.com>
2022-08-06 21:28:53 +0800

    第六集 事件 - 事件总览

--- src/main/java/io/github/myworldzycpc/hello_mod/Main.java
+++ src/main/java/io/github/myworldzycpc/hello_mod/Main.java
@@ -3,8 +3,14 @@ package io.github.myworldzycpc.hello_mod;
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,4 +26,30 @@ public class Main {
         player.sendMessage(new TextComponent("Hello, " + player.getDisplayName().getString() + ". From " + (level.isClientSide() ? "CLIENT" : "SERVER") + "."), Util.NIL_UUID);
     }
 
+    @SubscribeEvent
+    public static void leftClickBlock(PlayerInteractEvent.LeftClickBlock event) {
+        Player player = event.getPlayer();
+        player.sendMessage(new TextComponent("LeftCLickBlock is fired."), Util.NIL_UUID);
+    }
+
+    @SubscribeEvent
+    public static void rightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        Player player = event.getPlayer();
+        player.sendMessage(new TextComponent("RightClickBlock is fired."), Util.NIL_UUID);
+    }
+
+    @SubscribeEvent
+    public static void rightClickItem(PlayerInteractEvent.RightClickItem event) {
+        Player player = event.getPlayer();
+        Level level = player.level;
+        if (!level.isClientSide()) {
+            ItemStack itemStack = event.getItemStack();
+            Item item = itemStack.getItem();
+            if (item == Items.DIAMOND_SWORD){
+                HitResult hitResult = player.pick(20, 0, false);
+                Vec3 location = hitResult.getLocation();
+                player.teleportTo(location.x, location.y, location.z);
+            }
+        }
+    }
 }
